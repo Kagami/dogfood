69be51ddf38c0d2e14d13cc99baf9253c630efe5
Kagami Hiiragi <kagami@genshiken.org>
2019-10-17 02:57:33 +0300

    Colorful errors

--- dfcc/error.c
+++ dfcc/error.c
@@ -1,5 +1,17 @@
 #include "dfcc.h"
 
+static char gColorBlack[]   = { 27, '[', '3', '0', ';', '1', 'm', 0 };
+static char gColorRed[]     = { 27, '[', '3', '1', ';', '1', 'm', 0 };
+static char gColorMagenta[] = { 27, '[', '3', '5', ';', '1', 'm', 0 };
+static char gColorCyan[]    = { 27, '[', '3', '6', ';', '1', 'm', 0 };
+static char gColorReset[]   = { 27, '[', '0', 'm', 0 };
+
+typedef enum {
+  LogLevelNote,
+  LogLevelWarning,
+  LogLevelError,
+} LogLevel;
+
 typedef struct {
   char *filename;
   char *user_input;
@@ -17,7 +29,7 @@ void error_init(char *filename, char *user_input) {
 //
 // foo.c:10: x = y + 1;
 //               ^ <error message here>
-static void verror_at(char *loc, char *fmt, va_list ap) {
+static void verror_at(LogLevel lvl, char *loc, char *fmt, va_list ap) {
   // TODO(Kagami): Bound checking.
   // Find a line containing `loc`.
   char *line = loc;
@@ -35,15 +47,49 @@ static void verror_at(char *loc, char *fmt, va_list ap) {
       line_num++;
 
   // Print out the line.
+  fprintf(stderr, "%s", gColorBlack);
   int indent = fprintf(stderr, "%s:%d: ", gCtx->filename, line_num);
-  fprintf(stderr, "%.*s\n", (int)(end - line), line);
+  fprintf(stderr, "%s%.*s\n", gColorReset, (int)(end - line), line);
 
   // Show the error message.
   int pos = loc - line + indent;
   fprintf(stderr, "%*s", pos, ""); // print pos spaces.
-  fprintf(stderr, "^ ");
+  switch (lvl) {
+  case LogLevelNote:
+    fprintf(stderr, "%s^ note: %s", gColorCyan, gColorReset);
+    break;
+  case LogLevelWarning:
+    fprintf(stderr, "%s^ warning: %s", gColorMagenta, gColorReset);
+    break;
+  default:
+    fprintf(stderr, "%s^ error: %s", gColorRed, gColorReset);
+    break;
+  }
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
+}
+
+// Reports an error location and exit.
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  verror_at(LogLevelError, loc, fmt, ap);
+  exit(1);
+}
+
+// Reports an error location and exit.
+void error_tok(Token *tok, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  verror_at(LogLevelError, tok->str, fmt, ap);
+  exit(1);
+}
+
+// Reports an warning location.
+void warn_tok(Token *tok, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  verror_at(LogLevelWarning, tok->str, fmt, ap);
 }
 
 // Reports an error and exit.
@@ -53,27 +99,4 @@ void error(char *fmt, ...) {
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
-}
-
-// Reports an error location and exit.
-void error_at(char *loc, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  verror_at(loc, fmt, ap);
-  exit(1);
-}
-
-// Reports an error location and exit.
-void error_tok(Token *tok, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  verror_at(tok->str, fmt, ap);
-  exit(1);
-}
-
-// Reports an warning location.
-void warn_tok(Token *tok, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  verror_at(tok->str, fmt, ap);
 }
