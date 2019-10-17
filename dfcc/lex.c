b06bdee511cc553b5dd5aebad469bbd721034222
Kagami Hiiragi <kagami@genshiken.org>
2019-10-17 10:35:07 +0300

    Fix lex function names

--- dfcc/lex.c
+++ dfcc/lex.c
@@ -14,12 +14,12 @@ static bool startswith(char *p, char *q) {
   return strncmp(p, q, strlen(q)) == 0;
 }
 
-static bool is_alpha(char c) {
+static bool is_ident_start(char c) {
   return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
 }
 
-static bool is_alnum(char c) {
-  return is_alpha(c) || ('0' <= c && c <= '9');
+static bool is_ident(char c) {
+  return is_ident_start(c) || isdigit(c);
 }
 
 static char *starts_with_reserved(char *p) {
@@ -32,7 +32,7 @@ static char *starts_with_reserved(char *p) {
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++) {
     int len = strlen(kw[i]);
-    if (startswith(p, kw[i]) && !is_alnum(p[len]))
+    if (startswith(p, kw[i]) && !is_ident(p[len]))
       return kw[i];
   }
 
@@ -120,10 +120,10 @@ static Token *read_int_literal(Token *cur, char *start) {
 
   // Read a binary, octal, decimal or hexadecimal number.
   int base;
-  if (!strncasecmp(p, "0x", 2) && is_alnum(p[2])) {
+  if (!strncasecmp(p, "0x", 2) && is_ident(p[2])) {
     p += 2;
     base = 16;
-  } else if (!strncasecmp(p, "0b", 2) && is_alnum(p[2])) {
+  } else if (!strncasecmp(p, "0b", 2) && is_ident(p[2])) {
     p += 2;
     base = 2;
   } else if (*p == '0') {
@@ -146,7 +146,7 @@ static Token *read_int_literal(Token *cur, char *start) {
     ty = gLongType;
   }
 
-  if (is_alnum(*p))
+  if (is_ident(*p))
     error_at(p, "invalid digit");
 
   Token *tok = new_token(TK_NUM, cur, start, p - start);
@@ -209,9 +209,9 @@ Token *tokenize(char *user_input) {
     }
 
     // Identifier
-    if (is_alpha(*p)) {
+    if (is_ident_start(*p)) {
       char *q = p++;
-      while (is_alnum(*p))
+      while (is_ident(*p))
         p++;
       cur = new_token(TK_IDENT, cur, q, p - q);
       continue;
